964c64f2649b70ec1aae28f23c1cf7d2f9394376
Nikita Lugovykh <65580805+NikitaLug@users.noreply.github.com>
2021-08-31 18:01:19 +0200

    add files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Menu from './components/Menu';
+
+import { data } from "./data.js";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Menu items={data} title = 'Delicious Recipes '/>, 
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
